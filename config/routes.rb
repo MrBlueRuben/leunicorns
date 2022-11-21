b9a1e8c05066d5c0d6cfbc19cf2456fce87935bb
Felipe Hernandez <felipe2eu@gmail.com>
2022-11-21 17:43:08 +0100

    router

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :unicorns
+  resources :unicorns, only: %i[index show new create destroy] do
+    resources :bookings, only: %i[new create index edit update]
+  end
+    resources :bookings, only: [ :destroy ]
 end

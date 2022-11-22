4ecd38d1a0ec7d902506a114cadefc046fd4c7ee
ruben sanchez <sanchez.alvarez.ruben@gmail.com>
2022-11-22 12:02:02 +0100

    done controllerUnicorn

--- app/controllers/unicorns_controller.rb
+++ app/controllers/unicorns_controller.rb
@@ -5,4 +5,41 @@ class UnicornController < ApplicationController
   def index
     @unicorns = Unicorn.all
   end
+
+  def new
+    @unicorn = Unicorn.new
+  end
+
+  def create
+    @unicorn = Unicorn.new(unicorn_params)
+
+    if @unicorn.save
+      redirect_to unicorn_path(@unicorn)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @unicorn.update(booking_params)
+    redirect_to booking_path(@unicorn)
+  end
+
+  def destroy
+    @unicorn.destroy
+    redirect_to unicorn_path(@unicorn), notice: 'unicorn was successfully destroyed'
+  end
+
+  private
+
+  def set_unicorn
+    @unicorn = Unicorn.find(params[:id])
+  end
+
+  def unicorn_params
+    params.require(:unicorn).permit(:name, :location, :spicies, :user_id)
+  end
 end

e471e81726166b5690e6044b7073b7f884ab197d
Felipe Hernandez <felipe2eu@gmail.com>
2022-11-22 11:11:09 +0100

    bookings controller and model

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,46 @@
+class BookingsController < ApplicationController
+  before_action :set_unicorn, only: %i[new create update show edit destroy]
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.unicron = @unicorn
+    if @booking.save
+      redirect_to list_path(@unicorn)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+
+    redirect_to booking_path(@booking)
+  end
+
+  def destroy
+    @booking.destroy
+
+    redirect_to list_path(@booking), notice: 'booking was successfully destroyed'
+  end
+
+  private
+
+  def set_unicorn
+    @unicorn = Unicorn.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date, :unicorn_id, :user_id)
+  end
+end

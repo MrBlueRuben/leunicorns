e88538e3ea24bcfc1f5fd27dc4bef8273298e402
Felipe Hernandez <felipe2eu@gmail.com>
2022-11-23 14:04:06 +0100

    index page for unicorns

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :unicorns, dependent: :destroy
   has_many :bookings, dependent: :destroy
 
-  validates :name, uniqueness: true, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
